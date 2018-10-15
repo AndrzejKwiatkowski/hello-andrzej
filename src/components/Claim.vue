000ef1d7dd99bdb628b59657f8ca93f9857dca2f
AndrzejKwiatkowski <andrzej_kwiatkowski@outlook.com>
2018-10-15 21:15:49 +0200

    stylowanie apki

--- src/components/Claim.vue
+++ src/components/Claim.vue
@@ -1,30 +1,34 @@
 <template>
-<div class="wrapper">
+  <div class="claimWrapper">
     <h1 class="claim">SPACER</h1>
-    <p class="subclaim">Begin your journey through our amazing galaxy,
+    <p class="subclaim">
+      Begin your journey through our amazing galaxy,
       and discover places you never even heard of.
     </p>
     <p class="subclaim">
-        Type anything space-related to start.
+      Type anything space-related to start.
     </p>
-</div>
-    
+  </div>
 </template>
-
 <script>
 export default {
-name: 'Claim',
-}
+  name: 'Claim',
+};
 </script>
-
 <style lang="scss" scoped>
-.wrapper {
+  .claimWrapper {
     display: flex;
+    text-align: center;
     flex-direction: column;
     justify-content: center;
     align-items: center;
-    
-
-}
-
+  }
+  .claim {
+    font-size: 50px;
+    margin: 0;
+  }
+  .subclaim {
+    margin: 20px 0 0 0;
+    font-size: 13px;
+  }
 </style>
