c5e6d17c39af2c31503119ea94c86fdbf541a964
AndrzejKwiatkowski <andrzej_kwiatkowski@outlook.com>
2018-10-17 18:24:47 +0200

    stylowanie, usuniecie router, podział na komponenty

--- src/components/SearchInput.vue
+++ src/components/SearchInput.vue
@@ -20,10 +20,22 @@ export default {
     flex-direction: column;
     width: 250px;
     input {
+      color: white;
+      text-align: center;
+      font-size: 18px;
+      font-weight: 300;
       height: 30px;
       border: 0;
       background: none;
-      border-bottom: 1px solid black;
+      border-bottom: 1px solid white;
+      transition: box-shadow .3s ease-out;
+      @media (min-width: 1024px) {
+        font-weight: 400;
+      }
+    }
+    input:focus {
+      outline: none;
+      box-shadow: 0 10px 20px -8px rgba(255,255,255, .5);
     }
   }
 </style>
